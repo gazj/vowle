26bddf0974d07b6b78beb012fea493d88da4eeb6
gazj <gazzzzzj@gmail.com>
2024-03-10 10:36:23 -0400

    Fix formatting in share clipboard text

--- src/components/widgets/Share.svelte
+++ src/components/widgets/Share.svelte
@@ -15,7 +15,7 @@
 			}/${state.board.words.length}\n\n    ${state.board.state
 				.slice(0, state.guesses)
 				.map((r) => r.join(""))
-				.join("\n    ")}\gazj.github.io/vowle`
+				.join("\n    ")}\n\ngazj.github.io/vowle`
 		);
 		toaster.pop("Copied");
 	}
