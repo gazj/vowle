f88d9d855b2379bb06109a1824547014b96d1b2d
gazj <gazzzzzj@gmail.com>
2024-03-11 10:10:19 -0400

    Update project to v1.0.2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-//! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 import App from "./App.svelte";
 
 const app = new App({
 	target: document.body,
 	props: {
-		version: "1.0.1",
+		version: "1.0.2",
 	}
 });
 
